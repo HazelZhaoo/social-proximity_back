ae8f75dab3ce56585c90f4f47b205fdf1c90382e
Duc Ng <duc.nguyen@weallnet.com>
2025-03-22 00:39:57 -0400

    update

--- views/LocationView.py
+++ views/LocationView.py
@@ -1,10 +1,20 @@
-from fastapi import APIRouter
-
+from fastapi import APIRouter, HTTPException, Depends, status
+from models.Database import get_db, get_mongo_db
+from motor.motor_asyncio import AsyncIOMotorDatabase
+from sqlalchemy.orm import Session
 
 router = APIRouter()
 
 
-@router.post("/save-gps")
-async def save_gps():
+@router.get("/health-check")
+async def health_check():
+    return {"status": "ok"}
 
+
+@router.post("/save-gps")
+async def save_gps(
+    # db: Session = Depends(get_db),
+    mongo_db: AsyncIOMotorDatabase = Depends(get_mongo_db),
+):
+    print("GPS Data Saved")
     return
