9c43c86ee12b7ddc0a9f9a48a4c3874d12b6726f
Duc Ng <duc.nguyen@weallnet.com>
2025-03-21 22:59:42 -0400

    update Location View

---
+++ controllers/LocationController.py
@@ -0,0 +1,11 @@
+from sqlalchemy.orm import Session
+from motor.motor_asyncio import AsyncIOMotorDatabase
+
+async def save_location(
+    db: Session ,
+    mongo_db: AsyncIOMotorDatabase,
+): 
+    
+    
+    
+    return 
---
+++ views/LocationView.py
@@ -0,0 +1,10 @@
+from fastapi import APIRouter
+
+
+router = APIRouter()
+
+
+@router.post("/save-gps")
+async def save_gps():
+
+    return
